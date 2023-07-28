045b76b2036e252e97ad6815155bcf9cadbb922a
abror2007-ux <abrorrustamov446@gmail.com>
2023-07-28 17:26:17 +0500

    coder ar

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var eBtn = document.querySelector(".btn");
+var elInp = document.querySelector(".inp");
+var elHead = document.querySelector(".head");
+function fn() {
+  if (elInp.value > 9250000) {
+    elHead.textContent = "Alisher oq yol";
+    elInp.value = "";
+  } else if (elInp.value < 9250000) {
+    elHead.textContent = "Alisher damin olaver";
+    elInp.value = "";
+  } else {
+    elHead.textContent = "Faqat raqam kriting";
+    elInp.value = "";
+  }
+}
